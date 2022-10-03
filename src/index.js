ad3cc004998a62998676ca1b8fd76ed42ccbfe07
abyzgareeva <mess.mes16@gmail.com>
2022-10-03 21:00:22 +0300

    Added seaqrch field component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import SearchField from './components/search_field/search_field';
+import SearchField from './components/search_field/search_field';
 //import Main from './pages/main/main';
-import Header from './components/header/header';
+//import Header from './components/header/header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Header />
+  <SearchField />
 );
 
