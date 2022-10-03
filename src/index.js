550eafd07efbc83270dd8805a06ac26df26de369
abyzgareeva <mess.mes16@gmail.com>
2022-10-03 23:42:16 +0300

    Added recomendation card component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchField from './components/search_field/search_field';
+//import SearchField from './components/search_field/search_field';
 //import Main from './pages/main/main';
 //import Header from './components/header/header';
-
+import RecomentationPost from './components/card/card';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <SearchField />
+  <RecomentationPost />
 );
 
