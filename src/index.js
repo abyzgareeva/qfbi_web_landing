45888f0f8d4d1bf120dab30e91b7bcc542267aa1
abyzgareeva <mess.mes16@gmail.com>
2022-10-03 22:52:21 +0300

    Updated dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import SearchField from './components/search_field/search_field';
 //import Main from './pages/main/main';
 //import Header from './components/header/header';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <SearchField />
